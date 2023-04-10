ba1f5bdd455950703f5be8aeb8ed40be8facc40f
mopifish <87352493+mopifish@users.noreply.github.com>
2023-04-10 12:23:59 -0700

    Header Comment
    Added header comment

--- Lab 1 - Classes in C++/source/main.cpp
+++ Lab 1 - Classes in C++/source/main.cpp
@@ -1,3 +1,9 @@
+// Drake Pickett
+// 4.10.2023
+// CS 145 - Lab 1
+// A simple Rectangle class based off of exercise 9.11 in the book.
+// Seeks to show headers vs implementation, and encapsulation of data using classes.
+
 #include <iostream>
 #include "Rectangle.h"
 
