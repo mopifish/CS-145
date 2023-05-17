f6249ed4c1849f8f94fccc2de88a1de1969a7c96
mopifish <87352493+mopifish@users.noreply.github.com>
2023-05-17 10:44:48 -0700

    Upload WordSearch
    Initial commit for my word search assignment

---
+++ Word Search Generator/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "WordSearch.h"
+
+int main() {
+    WordSearch ws = WordSearch({"Carl", "Alien", "Nolan", "Ignoramus", "Champion", "Alfred"});
+    std::cout << ws;
+
+    return 0;
+}
